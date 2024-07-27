9ef5f7f33184fe6e8b51a47aae0aa4340736b04f
JustFavour <odeyemifavour1@gmail.com>
2024-07-27 09:23:05 +0000

    feat: verify and sign signature

--- string_util.py
+++ string_util.py
@@ -1,15 +1,39 @@
-from hashlib import sha256
+import hashlib
+from Crypto.Signature import DSS
+from Crypto.Hash import SHA256
 from Crypto.PublicKey import ECC
-import base64
+from typing import Union
 
 class string_util:
-    
-    def apply_sha_256( self, strinput: str) -> str:
-        try:
-            hash :str = sha256(strinput.encode('utf-8')).hexdigest()
-            return hash 
-        except Exception as e: 
-            raise RuntimeError(e)
-        
+    @staticmethod
     def get_string_from_key(key: ECC.EccKey) -> str:
-        return base64.b64encode(key.export_key(format='DER')).decode('utf-8')
+        return key.export_key(format='DER').hex()
+
+# a helper function to get create digital signature 
+    @staticmethod
+    def apply_ECDSA_sig(private_key: ECC.EccKey, data: str) -> bytes:
+        data_hash = SHA256.new(data.encode('utf-8'))
+        signer = DSS.new(private_key, 'fips-186-3')
+        return signer.sign(data_hash)
+    
+ # a helper function to verify signature 
+    @staticmethod
+    def verify_ECDSA_sig(public_key: ECC.EccKey, data: str, signature: bytes) -> bool:
+        data_hash = SHA256.new(data.encode('utf-8'))
+        verifier = DSS.new(public_key, 'fips-186-3')
+        try:
+            verifier.verify(data_hash, signature)
+            return True
+        except ValueError:
+            return False
+                            
+    # Converts input string to bytes        
+    # Creates a new SHA-256 hash object and update it with the input bytes
+    @staticmethod
+    def apply_sha_256(strinput: str) -> str:
+        try:
+            hash_str = hashlib.sha256(strinput.encode('utf-8')).hexdigest()
+            return hash_str
+        except Exception as e:
+            raise RuntimeError(e)
+
